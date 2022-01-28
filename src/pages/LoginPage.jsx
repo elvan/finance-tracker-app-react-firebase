550a111790367e12ad94553185fc2adfa8dc3b5b
Elvan H <elvanhd@gmail.com>
2022-01-28 19:31:27 +0700

    menggunakan hook useLogin

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,43 +1,50 @@
 import { useState } from 'react';
+import { useLogin } from '../hooks/useLogin';
 import styles from './LoginPage.module.css';
 
 export default function LoginPage() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
+  const { pending, error, login } = useLogin();
+
   const handleSubmit = (event) => {
     event.preventDefault();
 
-    console.log('email:', email);
-    console.log('password:', password);
+    login(email, password);
   };
 
   return (
     <div>
       <form className={styles.loginForm} onSubmit={handleSubmit}>
         <h2>Login</h2>
+
+        {error.message && <p style={{ color: 'red' }}>{error.message}</p>}
+
         <label htmlFor='email'>
           <span>Email</span>
           <input
+            value={email}
             id='email'
             type='email'
             name='email'
-            value={email}
+            disabled={pending}
             onChange={(event) => setEmail(event.target.value)}
           />
         </label>
         <label htmlFor='password'>
           <span>Password</span>
           <input
+            value={password}
             id='password'
             name='password'
             type='password'
-            value={password}
+            disabled={pending}
             onChange={(event) => setPassword(event.target.value)}
           />
         </label>
-        <button className='btn' type='submit'>
-          Login
+        <button className='btn' type='submit' disabled={pending}>
+          {pending ? 'Logging in...' : 'Login'}
         </button>
       </form>
     </div>

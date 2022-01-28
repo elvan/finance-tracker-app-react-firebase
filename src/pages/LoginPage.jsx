734d5de23850dcf5494b7013fff8bbf8df923ed1
Elvan H <elvanhd@gmail.com>
2022-01-28 14:37:43 +0700

    membuat login form

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,3 +1,45 @@
+import { useState } from 'react';
+import styles from './LoginPage.module.css';
+
 export default function LoginPage() {
-  return <div>LoginPage</div>;
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    console.log('email:', email);
+    console.log('password:', password);
+  };
+
+  return (
+    <div>
+      <form className={styles.loginForm} onSubmit={handleSubmit}>
+        <h2>Login</h2>
+        <label htmlFor='email'>
+          <span>Email</span>
+          <input
+            id='email'
+            type='email'
+            name='email'
+            value={email}
+            onChange={(event) => setEmail(event.target.value)}
+          />
+        </label>
+        <label htmlFor='password'>
+          <span>Password</span>
+          <input
+            id='password'
+            name='password'
+            type='password'
+            value={password}
+            onChange={(event) => setPassword(event.target.value)}
+          />
+        </label>
+        <button className='btn' type='submit'>
+          Login
+        </button>
+      </form>
+    </div>
+  );
 }

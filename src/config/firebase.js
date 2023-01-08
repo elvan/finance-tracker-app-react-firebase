4306f67a3df0f0a7a2629822fe80d8478852f56c
Elvan Hidayat <elvanhd@gmail.com>
2023-01-08 12:13:26 +0700

    update project setup

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,15 +1,7 @@
 import firebase from 'firebase/app';
 import 'firebase/auth';
 import 'firebase/firestore';
-
-const firebaseConfig = {
-  apiKey: 'AIzaSyCFMADaHSuJb7_OGSI8fDjnArSKlYRMSjU',
-  authDomain: 'finance-tracker-app-react.firebaseapp.com',
-  projectId: 'finance-tracker-app-react',
-  storageBucket: 'finance-tracker-app-react.appspot.com',
-  messagingSenderId: '886033841873',
-  appId: '1:886033841873:web:5a269afb8b68442bc00ff0',
-};
+import { firebaseConfig } from '../firebaseConfig';
 
 firebase.initializeApp(firebaseConfig);
 
